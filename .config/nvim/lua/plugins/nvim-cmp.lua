1f5cec2b61c149c968f91ea2f0543955a50d6672
Ross Carrick <rosshughcarrick@gmail.com>
2024-12-16 12:15:40 +0000

    VIM | Don't select first cmp option

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -27,7 +27,7 @@ return {
 				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
 			}),
 			-- sources for autocompletion
 			sources = cmp.config.sources({
