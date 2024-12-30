192c682a984ba89b0186330059b299519c0bf86b
Ross Carrick <rosshughcarrick@gmail.com>
2024-12-30 21:32:57 +0000

    VIM | Formatting

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,20 +1,32 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	opts = {
-		defaults = {
-			prompt_prefix = "",
-			selection_caret = "> ",
-			layout_config = {
-				horizontal = {
-					width = 0.9,
-					preview_width = 0.5,
-				},
-			},
-		},
-	},
-	keys = {
-		{ "<Leader>p", ":Telescope find_files<CR>", { noremap = true, silent = true } },
-		{ "<Leader>f", ":Telescope live_grep<CR>", { noremap = true, silent = true } },
-		{ "<Leader>b", ":Telescope buffers<CR>", { noremap = true, silent = true } },
-	},
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    defaults = {
+      prompt_prefix = "> ",
+      selection_caret = "> ",
+      layout_config = {
+        horizontal = {
+          width = 0.9,
+          preview_width = 0.5,
+        },
+      },
+    },
+  },
+  keys = {
+    {
+      "<Leader>p",
+      ":Telescope find_files<CR>",
+      { noremap = true, silent = true },
+    },
+    {
+      "<Leader>f",
+      ":Telescope live_grep<CR>",
+      { noremap = true, silent = true },
+    },
+    {
+      "<Leader>b",
+      ":Telescope buffers<CR>",
+      { noremap = true, silent = true },
+    },
+  },
 }
