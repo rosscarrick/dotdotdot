e97f6dd2a280b79cb5327fc81c97fccb4f26f1ff
Ross Carrick <rosshughcarrick@gmail.com>
2025-01-14 00:30:49 +0000

    Formatting

--- .config/nvim/lua/plugins/formatter.lua
+++ .config/nvim/lua/plugins/formatter.lua
@@ -1,32 +1,33 @@
 return {
-	"stevearc/conform.nvim",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local conform = require("conform")
-		local webConfig = { "prettier" }
+  "stevearc/conform.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  config = function()
+    local conform = require("conform")
+    local webConfig = { "prettier" }
 
-		conform.setup({
-			formatters_by_ft = {
-				javascript = webConfig,
-				typescript = webConfig,
-				javascriptreact = webConfig,
-				typescriptreact = webConfig,
-				svelte = webConfig,
-				css = webConfig,
-				html = webConfig,
-				json = webConfig,
-				yaml = webConfig,
-				markdown = webConfig,
-				lua = { "stylua" },
+    conform.setup({
+      formatters_by_ft = {
+        javascript = webConfig,
+        typescript = webConfig,
+        javascriptreact = webConfig,
+        typescriptreact = webConfig,
+        svelte = webConfig,
+        css = webConfig,
+        html = webConfig,
+        xml = webConfig,
+        json = webConfig,
+        yaml = webConfig,
+        markdown = webConfig,
+        lua = { "stylua" },
 
-				-- don't forget to install shfmt (brew install shfmt)
-				sh = { "shfmt" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 2000,
-			},
-		})
-	end,
+        -- don't forget to install shfmt (brew install shfmt)
+        sh = { "shfmt" },
+      },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 2000,
+      },
+    })
+  end,
 }
